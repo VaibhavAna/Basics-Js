cb37216416f5228c4be22fb7b7db7c7799f7871a
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2023-12-29 20:05:33 +0000

    config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountemail ="vaibhavanand2494@gmail.com"
+var accountpasswrd ="1234"
+accountcity = "jaipur"
+
+
+accountemail="anand234@gmail.com"
+accountpasswrd="123"
+console.log(accountId)
+
+console.table([accountId,accountemail,accountcity,accountpasswrd])
+
+
+/*
+
+prefer not to use  var
+because of issues in block scope and function scope
+*/
