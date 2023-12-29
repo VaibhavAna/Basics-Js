cf62cca7b5a666057c7cea6289e9357aead4b29b
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2023-12-29 20:07:29 +0000

    config

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,36 @@
+const name="vaibh"
+const repoCount=30
+
+//console .log(name+repoCount+"value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('vaibhhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherstring=gameName.slice(-8,4)
+console.log(anotherstring);
+
+/*const anothernewstring=gameName.matchAll['5']
+console.log(anothernewstring);*/
+
+const newstringOne="  vaibh  "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+
+const url="https://vaibh.com"
+console.log (url.replace('%20','-'));
+
+
+
