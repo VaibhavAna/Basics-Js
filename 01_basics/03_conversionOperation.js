3df91f4a21b1311f62bd7f693239ea7bf9dec90c
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2023-12-29 20:06:35 +0000

    config

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 123
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abs"=>NaN
+// true=>1;false=>0
+
+let isLoggedIn="vaibh"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+// ""=>false
+//"vaibh"=>true
+
+
+//************************************************operations******************************************
+
+
+let value =4
+let negvalue=-value
+console.log(negvalue);
