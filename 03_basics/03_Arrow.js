54a587bc11751992137d28a72a50a6285045fc15
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2023-12-31 17:10:46 +0000

    config

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -3,13 +3,32 @@ const user={
     price:999,
 
     welcomemessage:function(){
-        console.log(`${this.username},welcome to website`);
-
+        //console.log(`${this.username},welcome to website`);
+//console.log(this);
     }
 }
 user.welcomemessage()
+
 
 /*function duo(){
     let username="vaibh"
     console.log(this.username);
 }*/
+const chai=() => {
+    let username="vaibh"
+   //console.log(this.username);
+}
+
+/*const addTwo =(num1,num2) =>({username:"vaibh"})
+console.log(addTwo(3,4))*/
+
+
+
+
+
+//implicit return is used when function run without using return //
+
+//mostly used in react 
+const addTwo =(num1,num2) =>  num1 + num2
+
+console.log(addTwo(3,4))
