c0f68a3eabdce95960e341ee2c2562958feeb5f6
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2024-01-07 09:32:44 +0000

    commit

---
+++ 04_controlflow/Truthe.js
@@ -0,0 +1,26 @@
+const usermail="vaibh.ai"
+
+if(usermail){
+    console.log("got user mail");
+
+}else{
+    console.log("not have mail");
+}
+
+//false value
+
+false,0,-0,BigInt ,0n,"",null,undefined,NaN
+
+//truthy value
+
+//tuthy,"0",'false',"truthy",[],{},function(){}
+
+
+if (usermail.length===0){
+    console.log("array is empty");
+}
+
+const emptyobj={}
+if (Object.keys(emptyobj).length===0){
+    console.lof("obj is empty");
+}
