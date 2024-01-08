64d440788c8e5dc9fdda5a06329dd6ae69ff5198
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2024-01-08 09:12:04 +0000

    comp

---
+++ 05_iterations/four.js
@@ -0,0 +1,11 @@
+const coding=["js","python","ruby","cpp"]
+
+coding.forEach(function name (item) {
+
+    console.log(item)
+} )
+
+function printme(item){
+    console.log(item);
+}
+//coding.forEach(printme)
