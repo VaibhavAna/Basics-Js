7ecc1ae56a25aca7261514e94fe9f10d8f1b95a9
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2024-01-08 09:11:45 +0000

    config

---
+++ 05_iterations/three.js
@@ -0,0 +1,27 @@
+//For of loop
+
+//all loops are array specific loop 
+
+const array=[1,2,3,4,5]
+
+for (const num of array) {
+    console.log(num);
+    
+}
+
+const greetings="Hello"
+for (const greet of greetings) {
+    console.log(`each char is${greet}`)
+    
+}
+
+//Maps
+
+const map=new map()
+
+map.set(`IN`,`USA`,`PL`)
+map.set(`IR`,`SA`,`SL`)
+map.set(`RN`,`A`,`AL`)
+
+
+console.log(map);
