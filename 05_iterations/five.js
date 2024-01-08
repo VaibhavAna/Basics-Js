35e9a60f958b76ea3912a097017c125b8a4ff58f
Vaibhav Anand <136928412+VaibhavAna@users.noreply.github.com>
2024-01-08 09:27:14 +0000

    conditions

---
+++ 05_iterations/five.js
@@ -0,0 +1,13 @@
+const coding=["js","ruby","python","ruby","cpp"]
+
+const values=coding.forEach((item)=>{
+   // console.log(item);
+})
+
+console.log(values);
+
+
+const mynumbers=[1,2,3,4,5,6,7,8]
+
+const newnums = mynumbers.filter((num)=>num>4)
+console.log(newnums);
